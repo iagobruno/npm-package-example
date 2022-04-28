39acb9aad25e29c6170f85942259659e3509de6f
Iago Bruno <iagobruno.dev@gmail.com>
2022-04-28 13:14:38 -0300

    refactor: Extract repeted transform logic to separed file

--- src/snakeCase.ts
+++ src/snakeCase.ts
@@ -1,14 +1,8 @@
+import { transform } from './transform'
+
 export default function snakeCase(txt: string): string {
-  const transformed = txt
-    // "GET_NAME" -> "get_name"
-    .replace(/(?:_+)?([A-Z]+)/g, function (_, letter: string, at: number) {
-      return (at > 0 ? '_' : '') + letter
-    })
-    // "get_name" -> "get_name"
-    .replace(/(?:\s|-|_|\.|\/)/g, '_')
-    // "v 9.0" -> "v_9_0"
-    .replace(/(?:\.|\s)([0-9])/g, function (_, numb: string) {
-      return '_' + numb
-    })
+  const transformed = transform(txt, {
+    char: '_',
+  })
   return transformed.toLowerCase()
 }
